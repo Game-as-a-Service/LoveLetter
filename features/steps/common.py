b059ccfed152aa71d9292445fec05d20de88a50d
timk42 <alexlovesky@gmail.com>
2022-12-09 22:28:07 +0800

    test錯誤修正 錯誤訊息features/princess.feature:9  玩家被迫出牌 玩家出局 princess.feature中的 Then 玩家B 丟棄手牌 在 princess.feature 中沒有相對應的function 將 common.py @then('{player} 丟棄手牌') 加入後測試正常

--- features/steps/common.py
+++ features/steps/common.py
@@ -105,6 +105,12 @@ def player_discard_card(context, player: str, card: str):
     assert len(turn_player.cards) == 0
 
 
+@then('{player} 丟棄手牌')
+def player_discard_card(context, player: str):
+    turn_player: Player = getattr(context, player)
+    assert len(turn_player.cards) == 0
+
+
 @then('{player} 無法打出 {card}')
 def player_error_play_this_card(context, player: str, card: str):
     turn_player: Player = getattr(context, player)
