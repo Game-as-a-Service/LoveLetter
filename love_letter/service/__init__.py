d1d4dd210f5d2315abfba890d96787eeb71bed7b
Ching Yi, Chan <chingyichan.tw@gmail.com>
2022-12-11 12:06:02 +0800

    實作建立遊戲與加入遊戲 (#48)
    * Add create and join game to service、Add dev web-starter

--- love_letter/service/__init__.py
+++ love_letter/service/__init__.py
@@ -1,44 +1,43 @@
+import traceback
 from typing import Dict, Optional, Union
 
 from love_letter.models import Game, Player
+from love_letter.repository import GameRepository
 from love_letter.web.dto import GuessCard, ToSomeoneCard
-
-database = dict()
-
-
-def build_fake_data():
-    global database
-
-    game = Game()
-    game.id = 'g-5566'
-    database['g-5566'] = game
-
-    player_a = Player()
-    player_a.name = 'player-a'
-    player_a.cards = []
-    game.join(player_a)
-
-    player_b = Player()
-    player_b.name = 'player-b'
-    player_b.cards = []
-    game.join(player_b)
-
-
-build_fake_data()
 
 
 class GameService:
+
+    def __init__(self, repository: GameRepository):
+        self.repository = repository
+
+    def create_game(self, player_id: str) -> str:
+        game = Game()
+        # TODO for now, we dont have the registration process,
+        # TODO just create the player when they are creating or joining the game
+        game.join(Player.create(player_id))
+        return self.repository.save_or_update(game)
+
+    def join_game(self, game_id: str, player_id: str) -> bool:
+        try:
+            self.repository.get(game_id).join(Player.create(player_id))
+            return True
+        except BaseException as e:
+            traceback.print_exception(e)
+            return False
+
     def start_game(self, game_id: str) -> Optional[Dict]:
-        if game_id not in database:
+        game: Game = self.repository.get(game_id)
+        if game is None:
             return
-        game: Game = database.get(game_id)
         game.start()
         return game.to_dict()
 
     def play_card(self, game_id, player_id: str, card_name: str,
                   card_action: Union[GuessCard, ToSomeoneCard, None]) -> Optional[Dict]:
-        if game_id in database:
-            game: Game = database.get(game_id)
-            game.play(player_id, card_name, card_action)
-            return game.to_dict()
-        return
+
+        game: Game = self.repository.get(game_id)
+        if game is None:
+            return
+        game.play(player_id, card_name, card_action)
+        return game.to_dict()
