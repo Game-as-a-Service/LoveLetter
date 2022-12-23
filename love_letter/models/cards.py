2992bffcb30e7753e31d6ff7e642e43d59a9943a
Huang-Ming Chang <huang.m.chang@gmail.com>
2022-12-23 18:48:04 +0100

    fix: with_card cannot be ignored.

--- love_letter/models/cards.py
+++ love_letter/models/cards.py
@@ -85,7 +85,7 @@ class BaronCard(Card):
     value = 3
     quantity = 2
 
-    def trigger_effect(self, card_holder: "Player", chosen_player: "Player"):
+    def trigger_effect(self, card_holder: "Player", chosen_player: "Player", with_card: "Card" = None):
         if card_holder > chosen_player:
             chosen_player.out()
         else:
