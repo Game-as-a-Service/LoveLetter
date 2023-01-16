fd15fb57ff1658ada693402b33319a95362ead4b
Ching Yi, Chan <chingyichan.tw@gmail.com>
2023-01-17 00:29:07 +0800

    Add todo-notes
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,31 @@ import { useGameId, useUsername } from "./hooks";
 import { CreateOrJoinGame } from "./components/CreateOrJoinGame";
 import { GameStatus, ViewState } from "./types";
 import { GetGameStatus } from "./apis";
+import { isEqual } from "lodash";
 
 function GameRoom(props: { visitFunc: (view: ViewState) => void }) {
-  const [username, setUsername] = useUsername();
-  const [gameId, setGameId] = useGameId();
+  const [username] = useUsername();
+  const [gameId] = useGameId();
+  const [gameStatus, setGameStatus] = useState<GameStatus | null>();
 
+  // TODO 自動 refresh game status
+  // TODO 需要有 "開始遊戲" 的功能
   useEffect(() => {}, [
     GetGameStatus(gameId, username).then((status: GameStatus) => {
       // TODO status 中沒有等待的玩家列表，需要改 api
       console.log(status);
+      console.log(status.players[0].name);
+      if (!isEqual(gameStatus, status)) {
+        setGameStatus(status);
+      }
     }),
   ]);
 
+  // TODO 把 demo-sample 的圖搬進來
   return (
     <>
       <div>GameList: {username}</div>
+      <div>{JSON.stringify(gameStatus)}</div>
     </>
   );
 }
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,6 +1,11 @@
 export type ViewState = "pick-name" | "game-list" | "game-room";
 
+export interface NamedPlayer {
+  name: string;
+}
+
 export interface GameStatus {
   game_id: string;
+  players: Array<NamedPlayer>;
   rounds: [];
 }
