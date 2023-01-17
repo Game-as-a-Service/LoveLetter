b4e4ce9b8a51535ab43ee7b5f5310dc11f68f15d
Ching Yi, Chan <chingyichan.tw@gmail.com>
2023-01-17 18:41:09 +0800

    Connect GameStatus to status-board
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- frontend/src/components/GameRoom.tsx
+++ frontend/src/components/GameRoom.tsx
@@ -1,70 +1,69 @@
-import {GameStatus, ViewState} from "../types";
-import {useGameId, useUsername} from "../hooks";
-import React, {useEffect, useState} from "react";
-import {GetGameStatus} from "../apis";
-import {isEqual} from "lodash";
-import {PlayerHand} from "./PlayerHand";
-import {Deck} from "./Deck";
-import {PlayerList} from "./PlayerList";
-import {GameEvents} from "./GameEvents";
+import { GameStatus, ViewState } from "../types";
+import { useGameId, useUsername } from "../hooks";
+import React, { useEffect, useState } from "react";
+import { GetGameStatus } from "../apis";
+import { isEqual } from "lodash";
+import { PlayerHand } from "./PlayerHand";
+import { Deck } from "./Deck";
+import { GameStatusBoard } from "./GameStatusBoard";
+import { GameEvents } from "./GameEvents";
 
 export function GameRoom(props: { visitFunc: (view: ViewState) => void }) {
-    const [username] = useUsername();
-    const [gameId] = useGameId();
-    const [gameStatus, setGameStatus] = useState<GameStatus | null>();
+  const [username] = useUsername();
+  const [gameId] = useGameId();
+  const [gameStatus, setGameStatus] = useState<GameStatus | null>(null);
 
-    // TODO 自動 refresh game status
-    // TODO 需要有 "開始遊戲" 的功能
-    useEffect(() => {
-    }, [
-        GetGameStatus(gameId, username).then((status: GameStatus) => {
-            // TODO status 中沒有等待的玩家列表，需要改 api
-            console.log(status);
-            console.log(status.players[0].name);
-            if (!isEqual(gameStatus, status)) {
-                setGameStatus(status);
-            }
-        }),
-    ]);
+  // TODO 自動 refresh game status
+  // TODO 需要有 "開始遊戲" 的功能
+  useEffect(() => {}, [
+    GetGameStatus(gameId, username).then((status: GameStatus) => {
+      // TODO status 中沒有等待的玩家列表，需要改 api
+      console.log(status);
+      console.log(status.players[0].name);
+      if (!isEqual(gameStatus, status)) {
+        setGameStatus(status);
+      }
+    }),
+  ]);
 
-    return (
-        <>
-            {/*<div className="flex h-screen bg-slate-200">*/}
+  return (
+    <>
+      {/*<div className="flex h-screen bg-slate-200">*/}
 
-            {/*  <div>GameList: {username}</div>*/}
-            {/*  <div>{JSON.stringify(gameStatus)}</div>*/}
-            {/*</div>*/}
-            <div className="flex h-screen bg-slate-200">
-                <div className="w-[75vw] p-4 flex flex-col mx-auto">
-                    <div className="flex flex-grow items-center justify-center">
-                        <div className="flex h-[20vh]">
-                            <PlayerHand/>
-                        </div>
-                    </div>
-                    <div className="flex min-h-[38vh] items-center justify-center">
-                        <div className="flex h-[20vh] m-4">
-                            <PlayerHand/>
-                        </div>
-                        <div className="flex h-[20vh] w-[300px] m-4 ml-16 mr-16">
-                            <Deck></Deck>
-                        </div>
-                        <div className="flex h-[20vh] m-4">
-                            <PlayerHand/>
-                        </div>
-                    </div>
-                    <div className="flex flex-grow items-center justify-center">
-                        <div className="flex h-[20vh]">
-                            <PlayerHand/>
-                        </div>
-                    </div>
-                </div>
-                {/*<!-- Game Status-->*/}
-                <div className="w-[25vw] p-4 border-l-2 border-slate-400 shadow-amber-300">
-                    <PlayerList/>
-                    {/* <!-- Game events --> */}
-                    <GameEvents/>
-                </div>
+      {/*  <div>GameList: {username}</div>*/}
+      {/*  <div>{JSON.stringify(gameStatus)}</div>*/}
+      {/*</div>*/}
+      <div className="flex h-screen bg-slate-200">
+        <div className="w-[75vw] p-4 flex flex-col mx-auto">
+          <div className="flex flex-grow items-center justify-center">
+            <div className="flex h-[20vh]">
+              <PlayerHand />
             </div>
-        </>
-    );
+          </div>
+          <div className="flex min-h-[38vh] items-center justify-center">
+            <div className="flex h-[20vh] m-4">
+              <PlayerHand />
+            </div>
+            <div className="flex h-[20vh] w-[300px] m-4 ml-16 mr-16">
+              <Deck></Deck>
+            </div>
+            <div className="flex h-[20vh] m-4">
+              <PlayerHand />
+            </div>
+          </div>
+          <div className="flex flex-grow items-center justify-center">
+            <div className="flex h-[20vh]">
+              <PlayerHand />
+            </div>
+          </div>
+        </div>
+        {/*<!-- Game Status-->*/}
+        <div className="w-[25vw] p-4 border-l-2 border-slate-400 shadow-amber-300">
+          <GameStatusBoard gameStatus={gameStatus} />
+          {/* <!-- Game events --> */}
+          <GameEvents />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ frontend/src/components/GameStatusBoard.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { GameStatus } from "../types";
+
+function PlayerItem(props: { index: number; name: string }) {
+  if (props.name === "-") {
+    return (
+      <div className="bg-gray-400 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+        玩家{props.index}：-
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+      玩家{props.index}：{props.name}
+    </div>
+  );
+}
+
+export function GameStatusBoard(props: { gameStatus: GameStatus | null }) {
+  const { gameStatus } = props;
+  let gameProgress = "(未知)";
+
+  const data = [
+    { name: "-", index: 1 },
+    { name: "-", index: 2 },
+    { name: "-", index: 3 },
+    { name: "-", index: 4 },
+  ];
+
+  // game has not stared, use the top player list
+  if (gameStatus != null && gameStatus.rounds.length == 0) {
+    gameProgress = "等待玩家加入中...";
+    if (gameStatus.players.length >= 2) {
+      gameProgress = "等待遊戲開始";
+    }
+
+    gameStatus.players.map((p, idx) => {
+      data[idx] = { name: p.name, index: idx + 1 };
+    });
+  }
+
+  // TODO
+
+  return (
+    <>
+      <div className="mb-5">
+        <h1>遊戲狀態</h1>
+        <div className="bg-gray-100 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+          {gameProgress}
+        </div>
+        <h1>玩家列表</h1>
+        {data.map((x) => (
+          <PlayerItem index={x.index} name={x.name}></PlayerItem>
+        ))}
+      </div>
+    </>
+  );
+}
