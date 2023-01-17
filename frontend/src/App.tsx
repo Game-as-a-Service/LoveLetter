f35cebbc250bb35940643bbd016ede378ac81dca
Ching Yi, Chan <chingyichan.tw@gmail.com>
2023-01-17 18:06:03 +0800

    Refactoring: extract PlayerList
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { GetGameStatus } from "./apis";
 import { isEqual } from "lodash";
 import { CardBack, CardFront } from "./components/Cards";
 import { Deck } from "./components/Deck";
+import { PlayerList } from "./components/PlayerList";
 
 function GameEvents() {
   return (
@@ -19,28 +20,6 @@ function GameEvents() {
         公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹
       </div>
     </div>
-  );
-}
-
-function PlayerList() {
-  return (
-    <>
-      <div className="mb-5">
-        <h1>玩家列表</h1>
-        <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-          玩家1：-
-        </div>
-        <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-          玩家2：-
-        </div>
-        <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-          玩家3：-
-        </div>
-        <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-          玩家4：-
-        </div>
-      </div>
-    </>
   );
 }
 
---
+++ frontend/src/components/PlayerList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function PlayerList() {
+    return (
+        <>
+            <div className="mb-5">
+                <h1>玩家列表</h1>
+                <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                    玩家1：-
+                </div>
+                <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                    玩家2：-
+                </div>
+                <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                    玩家3：-
+                </div>
+                <div className="bg-amber-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                    玩家4：-
+                </div>
+            </div>
+        </>
+    );
+}
