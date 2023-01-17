f36cea4f827079d910218af04d9c0144b0ba63c5
Ching Yi, Chan <chingyichan.tw@gmail.com>
2023-01-17 18:06:56 +0800

    Refactoring: extract GameEvents
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,7 @@ import { isEqual } from "lodash";
 import { CardBack, CardFront } from "./components/Cards";
 import { Deck } from "./components/Deck";
 import { PlayerList } from "./components/PlayerList";
-
-function GameEvents() {
-  return (
-    <div>
-      <h1>遊戲事件</h1>
-      <div className="bg-blue-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-        公主發瘋惹
-      </div>
-      <div className="bg-blue-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
-        公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹
-      </div>
-    </div>
-  );
-}
+import { GameEvents } from "./components/GameEvents";
 
 function PlayerHand() {
   return (
---
+++ frontend/src/components/GameEvents.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function GameEvents() {
+    return (
+        <div>
+            <h1>遊戲事件</h1>
+            <div className="bg-blue-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                公主發瘋惹
+            </div>
+            <div className="bg-blue-200 m-4 p-2 min-h-[3rem] pl-3 rounded-xl flex items-center">
+                公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹、公主發瘋惹
+            </div>
+        </div>
+    );
+}
