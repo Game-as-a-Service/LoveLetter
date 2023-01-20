10c7ee6f0523e3e8df3dd6e4c318cec09eb7b422
Ching Yi, Chan <chingyichan.tw@gmail.com>
2023-01-20 21:11:44 +0800

    Fix tests
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,6 +4,8 @@ pip install --upgrade pip
 pip install poetry
 poetry update
 
+set -e
+
 export PYTHONPATH=$(pwd)
 poetry run py.test -v -s --cov=love_letter --cov-report term tests
 poetry run behave
