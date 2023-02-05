50ec8d8d27ba197c8d037902d283072a62c0c915
Eddy <eddy15201@gmail.com>
2023-02-05 15:21:35 +0800

    Add component key value

--- frontend/src/components/GameRoom.tsx
+++ frontend/src/components/GameRoom.tsx
@@ -70,9 +70,9 @@ export function GameRoom(props: { visitFunc: (view: ViewState) => void }) {
         </div>
         {/*<!-- Game Status-->*/}
         <div className="w-[25vw] p-4 border-l-2 border-slate-400 shadow-amber-300 max-h-[100vh]">
-          <GameStatusBoard />
+          <GameStatusBoard key={0}/>
           {/* <!-- Game events --> */}
-          <GameEvents />
+          <GameEvents key={0}/>
         </div>
       </div>
     </>
